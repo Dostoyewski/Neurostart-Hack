5e80d1c64743248dada64049b2926dcc3d4a2c95
Nick <lunyak.kolya@mail.ru>
2020-10-31 04:40:30 +0300

    fixing some dead buttons

--- lk/static/js/script.js
+++ lk/static/js/script.js
@@ -1,1 +1,7 @@
-M.AutoInit()
+let unassignedButtons = document.querySelectorAll('.unassigned')
+
+for (let each of unassignedButtons) {
+    each.addEventListener('mousedown', e => {
+        M.toast({html: 'Ой, но это пока не готово!'})
+    })
+}
